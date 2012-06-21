6b433a83e45d71a79201dc200cb176dcdde5729d
Robert Lemon <rob.lemon@gmail.com>
2012-06-21 14:26:15 -0400

    adding new meme images

--- v2/js/app.js
+++ v2/js/app.js
@@ -23,24 +23,33 @@ var font_list = $('#meme-fonts > li'),
 /* takes a string and a maxWidth and splits the text into lines */
 
 function fragmentText(text, maxWidth) {
-	var words = text.split(' '),
-		lines = [],
-		line = "";
-	if (text.length === 0 /* this should fix the crashing when you clear the input */ || ctx.measureText(text).width < maxWidth) {
-		return [text];
-	}
-	while (words.length > 0) {
-		if (ctx.measureText(words[0]).width >= maxWidth /* ugly ass hack fix (at least it does not crash)... do I wrap long words? or leave them to get cut off?? */ || ctx.measureText(line + words[0]).width < maxWidth) { 
-			line += words.shift() + " ";
-		} else {
-			lines.push(line);
-			line = "";
-		}
-		if (words.length === 0) {
-			lines.push(line);
-		}
-	}
-	return lines;
+    var words = text.split(' '),
+        lines = [],
+        line = "";
+    if (ctx.measureText(text).width < maxWidth) {
+        return [text];
+    }
+    while (words.length > 0) {
+        while (ctx.measureText(words[0]).width >= maxWidth) {
+            var tmp = words[0];
+            words[0] = tmp.slice(0, -1);
+            if (words.length > 1) {
+                words[1] = tmp.slice(-1) + words[1];
+            } else {
+                words.push(tmp.slice(-1));
+            }
+        }
+        if (ctx.measureText(line + words[0]).width < maxWidth) {
+            line += words.shift() + " ";
+        } else {
+            lines.push(line);
+            line = "";
+        }
+        if (words.length === 0) {
+            lines.push(line);
+        }
+    }
+    return lines;
 }
 
 /* Draw the canvas */
